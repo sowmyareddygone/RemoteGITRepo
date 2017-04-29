7c2dab9a95a773714d0f5358950b34d0fb2d3d0d
ctel-cpu-36 <ctel-cpu-36@ctel-cpu-36.ctelhyd.in>
2017-04-29 15:42:39 +0530

    modified

--- TestProj1/src/Sample.java
+++ TestProj1/src/Sample.java
@@ -3,7 +3,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 		System.out.println("Tseting");
-		System.out.println("Modified ");
+		System.out.println("Modified 123");
 
 	}
 
